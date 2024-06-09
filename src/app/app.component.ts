811f9017626daa1d35a0393ce5a52cce4f5989e7
Shruti-Singh-00 <ishrutisingh1010@gmail.com>
2024-06-09 14:05:06 +0530

    Navigation Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FontAwesomeModule],
+  imports: [RouterOutlet, HeaderComponent, FontAwesomeModule, NavigationbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
