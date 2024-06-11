6dfe2161c16b6ebf9c79b181335ef6a1ac3b08f7
Shruti-Singh-00 <ishrutisingh1010@gmail.com>
2024-06-11 15:23:26 +0530

    SideNavigation Done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationbarComponent } from './components/navigationbar/navigationbar.component';
+import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NavigationbarComponent, FontAwesomeModule],
+  imports: [HeaderComponent, NavigationbarComponent, FontAwesomeModule, SidenavigationComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
